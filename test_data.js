691a5e0c695fc6a62bae9686e838fc1bb7c9aea7
xi455 <79513741+xi455@users.noreply.github.com>
2022-12-25 17:07:29 +0800

    Add files via upload

---
+++ test_data.js
@@ -0,0 +1,206 @@
+let Millet_datas = [
+    {
+        menu_id: 1,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/12u-Silver.png?width=140&height=140",
+        name: "Xiaomi 12T Pro",
+        price: "NT$19,999"
+    },
+    {
+        menu_id: 1,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/L12A-Blue.png?width=140&height=140",
+        name: "Xiaomi 12T",
+        price: "NT$16,999"
+    },
+    {
+        menu_id: 1,
+        img: "https://i01.appmifile.com/webfile/globalweb/zhutuhktw/xiaomi-12pro.png?width=140&height=140",
+        name: "Xiaomi 12 Pro 5G",
+        price: "NT$26,999"
+    },
+    {
+        menu_id: 1,
+        img: "https://i01.appmifile.com/webfile/globalweb/zhutuhktw/xiaomi-12.png?width=140&height=140",
+        name: "Xiaomi 12 5G",
+        price: "NT$23,999"
+    },
+    {
+        menu_id: 1,
+        img: "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1657001340.34658983.png?width=140&height=140",
+        name: "Xiaomi 12 Lite",
+        price: "NT$13,999"
+    },
+    {
+        menu_id: 1,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/Xiaomi-12X-5G.png?width=140&height=140",
+        name: "Xiaomi 12X 5G",
+        price: "NT$16999"
+    },
+    {
+        menu_id: 2,
+        img: "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1655438703.61665108.png?width=140&height=140",
+        name: "Redmi 10C",
+        price: "NT$ 3,999"
+    },
+    {
+        menu_id: 2,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/redmi10a.png?width=140&height=140",
+        name: "Redmi 10A",
+        price: "NT$3,499"
+    },
+    {
+        menu_id: 2,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/Banner/l19n-Silver.png?width=140&height=140",
+        name: "Redmi 10 5G",
+        price: "NT$6,499起"
+    },
+    {
+        menu_id: 2,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/K6P-blue.png?width=140&height=140",
+        name: "Redmi Note 11S",
+        price: "NT$6,999"
+    },
+    {
+        menu_id: 2,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/K16U-Blue.png?width=140&height=140",
+        name: "Redmi Note 11 Pro+ 5G",
+        price: "NT$ 11999"
+    },
+    {
+        menu_id: 3,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/K7BP-White.png?width=140&height=140",
+        name: "POCO M5 系列",
+        price: "NT$4,699起"
+    },
+    {
+        menu_id: 3,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/Banner/L10-black.png?width=140&height=140",
+        name: "POCO F4 GT",
+        price: "NT$ 19999"
+    },
+    {
+        menu_id: 3,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer/x4-gt-blue.png?width=140&height=140",
+        name: "POCO X4 GT",
+        price: "NT$ 11999"
+    },
+    {
+        menu_id: 3,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/K6P-blue.png?width=140&height=140",
+        name: "POCO X4 Pro 5G",
+        price: "NT$ 8499起"
+    },
+    {
+        menu_id: 3,
+        img: "https://i01.appmifile.com/webfile/globalimg/c40-yellow.png?width=140&height=140",
+        name: "POCO C40",
+        price: "NT$ 3999"
+    },
+    {
+        menu_id: 3,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/POCO-F4.png?width=140&height=140",
+        name: "POCO F4",
+        price: "NT$12999"
+    },
+    {
+        menu_id: 4,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/Banner/xunhuankongjing.png?width=140&height=140",
+        name: "Xiaomi 空氣循環淨化器",
+        price: "NT$ 11,995"
+    },
+    {
+        menu_id: 4,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/Banner/fengshan2pro.png?width=140&height=140",
+        name: "智慧直流變頻電風扇 2 Pro",
+        price: "NT$ 2,995"
+    },
+    {
+        menu_id: 4,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/Banner/s10.PNG?width=140&height=140",
+        name: "Xiaomi 掃拖機器人 S10",
+        price: "NT$7995"
+    },
+    {
+        menu_id: 4,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/Banner/g10.png?width=140&height=140",
+        name: "米家無線吸塵器 G10",
+        price: "NT$ 7,495"
+    },
+    {
+        menu_id: 4,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/40914418.png?width=140&height=140",
+        name: "米家無線吸塵器 mini",
+        price: "NT$ 1,095"
+    },
+    {
+        menu_id: 4,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/Banner/projector.png?width=140&height=140",
+        name: "小米智慧投影機 2",
+        price: "NT$ 14,995"
+    },
+    {
+        menu_id: 5,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/TV-55.png?width=140&height=140",
+        name: "小米智慧顯示器",
+        price: "NT$ 15999 起"
+    },
+    {
+        menu_id: 5,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/1605592817.5.png?width=140&height=140",
+        name: "攝影機",
+        price: "NT$ 665 起"
+    },
+    {
+        menu_id: 5,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/Banner/800.png?width=140&height=140",
+        name: "Redmi Buds 3 Pro",
+        price: "NT$ 1,595"
+    },
+    {
+        menu_id: 5,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/xiaomi-smart-band-7-pro.png?width=140&height=140",
+        name: "Xiaomi 手環7 Pro",
+        price: "NT$ 1,995"
+    },
+    {
+        menu_id: 5,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/wifi-range-extender-ac1200.png?width=140&height=140",
+        name: "WiFi 訊號延伸器 AC1200",
+        price: "NT$ 695"
+    },
+    {
+        menu_id: 5,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/xiaomi-smart-doorbell-3.png?width=140&height=140",
+        name: "Xiaomi 智慧門鈴 3",
+        price: "NT$1895"
+    },
+    {
+        menu_id: 6,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/4174289.png?width=140&height=140",
+        name: "33W 快速充電器 1A1C版",
+        price: "NT$325"
+    },
+    {
+        menu_id: 6,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/xiaomi-50w-wireless.png?width=140&height=140",
+        name: "直立風冷無線充電座 50W",
+        price: "NT$ 1395"
+    },
+    {
+        menu_id: 6,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/pms158677033447231966.png?width=140&height=140",
+        name: "小米延長線",
+        price: "NT$ 395"
+    },
+    {
+        menu_id: 6,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/pms_1578450158.98451188.png?width=140&height=140",
+        name: "小米行動電源 3 快充版",
+        price: "NT$ 435"
+    },
+    {
+        menu_id: 6,
+        img: "https://i01.appmifile.com/webfile/globalimg/summer-1/pic/3195200010.png?width=140&height=140",
+        name: "空淨濾芯 除甲醛增強版 S1",
+        price: "NT$ 795"
+    },
+]
